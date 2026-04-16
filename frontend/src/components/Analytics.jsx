519966e6a3583b5a22fffca52987c23774bae059
Hannia Escamilla Pérez <hanescper@gmail.com>
2026-04-16 16:59:10 -0600

    Edited management.jsx / management.css

--- frontend/src/components/Analytics.jsx
+++ frontend/src/components/Analytics.jsx
@@ -1,9 +1,19 @@
 import './styledComponents/analytics.css'
 
+
 function Analytics() {
     return(
         <div className='analytics-bg'>
             <h1>Analytics</h1>
+            <h3># sprint</h3>
+            <div>
+                <ul className='kpi-container'>
+                    <li className='kpi'><p>Promedio Tasks / dev</p></li>
+                    <li className='kpi'><p>Promedio de horas / dev</p></li>
+                    <li className='kpi'><p>Task Completed</p></li>
+                    <li className='kpi'><p>Horas Trabajadas totales</p></li>
+                </ul>
+            </div>
         </div>
     );
 }

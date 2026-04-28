d5e42cfabcff677aa7b94afa872ffdac0a60e57c
Susu9878 <jalonzo9878@gmail.com>
2026-04-28 09:59:43 -0600

    started: kpi service and controllers

---
+++ MtdrSpring/backend/src/main/java/com/DTO/TeamSprintKpiDTO.java
@@ -0,0 +1,9 @@
+package com.DTO;
+
+public class TeamSprintKpiDTO {
+    private Double avgTasksPerUser;
+    private Double avgHoursPerUser;
+    private Long totalTasksCompleted;
+    private Long totalTasksAssigned;
+    private Double totalHoursWorked;
+}
---
+++ MtdrSpring/backend/src/main/java/com/DTO/UserKpiDTO.java
@@ -0,0 +1,10 @@
+package com.DTO;
+
+public class UserKpiDTO {
+    private Integer userId;
+    private String username;
+    private Long tasksCompleted;
+    private Long tasksInProgress;
+    private Long tasksNotStarted;
+    private Double hoursWorked;
+}

a0220b75e713f2cd8bbc0346a3dfa39e937a7954
Hannia Escamilla Pérez <hanescper@gmail.com>
2026-06-06 13:33:37 -0600

    Fixed HourRegistry (untested)

--- MtdrSpring/backend/src/main/frontend/src/components/Hour_registry.jsx
+++ MtdrSpring/backend/src/main/frontend/src/components/Hour_registry.jsx
@@ -6,25 +6,42 @@ function Hour_Registry(){
     const [workDay, setworkDay] = useState('');
     const [taskId, setTaskId] = useState('');
     const [isPending, setIsPending] = useState(false);
-
     
     const handleSubmit = (e) => {
         e.preventDefault();
-        const registry = {hours,workDay,taskId}
-        setIsPending(true);
-        
-         fetch("http://localhost:8080/todolist/worklogs", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            
-            body: JSON.stringify(registry)
-            }).then(() => {
-                console.log('Hours registered');
-                setIsPending(false);
-            })
+        const registry = {
+            workedHours: Number(hours),
+            workedDay: `${workDay}T00:00:00Z`,
+            taskId: Number(taskId)
         }
+
+        try {
+            setIsPending(true);
+            
+             const response = fetch("http://localhost:8080/todolist/worklogs", 
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                body: JSON.stringify(registry)
+            });
+
+            if(!response.ok){
+                throw new Error(`HTTP Error: ${response.status}`);
+            }
+
+            console.log("Hours registered successfully");
+
+            setHours("");
+            setworkDay("");
+            setTaskId("");
+        } catch(error){
+            console.error("Error registering hours:", error);
+        }finally{
+            setIsPending(false);
+        }
+    };
 
     return(
         <div className="formContainer">
@@ -35,18 +52,21 @@ function Hour_Registry(){
                     type= "number"
                     value={hours}
                     onChange={(e) => setHours(e.target.value)}
+                    required
                 />
                 <p className="labelP"> Work day: </p>
                 <input
-                    type= "number"
+                    type= "date"
                     value={workDay}
                     onChange={(e) => setworkDay(e.target.value)}
+                    required
                 />
                 <p className="labelP"> Task ID: </p>
                 <input
                     type= "number"
                     value={taskId}
                     onChange={(e) => setTaskId(e.target.value)}
+                    required
                 />
                 {!isPending && <button className="hourBtn">Register hours</button>}
                 {isPending && <button disabled>Registering hours...</button>}

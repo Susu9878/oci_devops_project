701d8bada87c5b2e40f1d1f1562fa5b531d6fd1a
Hannia Escamilla Pérez <hanescper@gmail.com>
2026-06-07 15:45:57 -0600

    Create Task completed (untested)

--- MtdrSpring/backend/src/main/frontend/src/components/CreateTask.jsx
+++ MtdrSpring/backend/src/main/frontend/src/components/CreateTask.jsx
@@ -3,11 +3,186 @@ import "./styledComponents/createTask.css"
 
 
 function CreateTask() {
+  const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [storyP, setstoryP] = useState("");
+  const [expHrs, setExpHrs] = useState("");
+  const [levPr, setPriority] = useState("");
+  const [status, setStatus] = useState("");
+  const [start, setStartDate] = useState("");
+  const [username, setUsername] = useState("");
+  const [sprint,setSprint] = useState("");
+  const [isPending, setIsPending] = useState(false);
   
+  const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+    const taskLog = {
+      taskName: String(name),
+      description: String(desc),
+      storyPoints: Number(storyP),
+      expectedHours: Number(expHrs),
+      priority: String(levPr),
+      status: Number(status),
+      startDate: `${start}T00:00:00Z`,
+      user: String(username),
+      sprint: Number(sprint)
+    };
+
+        console.log("Sending:", taskLog);
+    
+        try {
+            setIsPending(true);
+            const response = await fetch(
+                "http://localhost:8080/todolist",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(taskLog)
+                }
+            );
+    
+            if (!response.ok) {
+                throw new Error(`HTTP Error: ${response.status}`);
+            }
+      
+            console.log("Task created successfully");
+      
+            // Clear form after successful registration
+            setName("");
+            setDesc("");
+            setstoryP("");
+            setExpHrs("");
+            setPriority("");
+            setStatus("");
+            setStartDate("");
+            setUsername("");
+            setSprint("");
+
+        } catch (error) {
+            console.error("Error creating task:", error);
+        } finally {
+            setIsPending(false);
+        }
+    };
+
+    const resetForm = () => {
+      setName("");
+      setDesc("");
+      setstoryP("");
+      setExpHrs("");
+      setPriority("");
+      setStatus("");
+      setStartDate("");
+      setUsername("");
+      setSprint("");
+    };
 
 
   return (
-      <div>
+      <div className="formContainer">
+        <h2>Create Task</h2>
+            <form onSubmit={handleSubmit}>
+                <p className="labelP">Task name:</p>
+                <input
+                    type="text"
+                    value={name}
+                    placeholder="Task name"
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
+
+                <p className="labelP">Description:</p>
+                <input
+                    type="text"
+                    value={desc}
+                    placeholder="Description of task"
+                    onChange={(e) => setDesc(e.target.value)}
+                    required
+                />
+
+                <p className="labelP">Story points:</p>
+                <input
+                    type="number"
+                    placeholder="Story points"
+                    value={storyP}
+                    onChange={(e) => setstoryP(e.target.value)}
+                    required
+                />
+
+                <p className="labelP">Expected hours:</p>
+                <input
+                    type="number"
+                    placeholder="Expected hours"
+                    value={expHrs}
+                    onChange={(e) => setExpHrs(e.target.value)}
+                    required
+                />
+
+                <p className="labelP">Priority:</p>
+                <select value={levPr} onChange={(e) => setPriority(e.target.value)} className="optStyle">
+                  <option value="LOWEST">LOWEST</option>
+                  <option value="LOW">LOW</option>
+                  <option value="MEDIUM">MEDIUM</option>
+                  <option value="HIGH">HIGH</option>
+                  <option value="CRITICAL">CRITICAL</option>
+                </select>
+
+                <p className="labelP">Status:</p>
+                <select value={status} onChange={(e) => setStatus(e.target.value)} className="optStyle">
+                  <option value="NOT_STARTED">NOT_STARTED</option>
+                  <option value="IN_PROGRESS">IN_PROGRESS</option>
+                  <option value="DONE">DONE</option>
+                  <option value="NOT_DONE">NOT_DONE</option>
+                </select>
+
+                <p className="labelP">Start date:</p>
+                <input
+                    type="date"
+                    value={start}
+                    placeholder="Beginning date"
+                    onChange={(e) => setStartDate(e.target.value)}
+                    required
+                />
+
+                <p className="labelP">User assigned:</p>
+                <input
+                    type="text"
+                    value={username}
+                    placeholder="Username"
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+
+                <p className="labelP">Sprint:</p>
+                <input
+                    type="number"
+                    value={sprint}
+                    placeholder="Sprint number"
+                    onChange={(e) => setSprint(e.target.value)}
+                    required
+                />
+                <div className="buttonContainer">
+                  {!isPending && (
+                      <button className="taskBtn">
+                          Create task
+                      </button>
+                  )}
+
+                  {isPending && (
+                      <button disabled>
+                          Creating task...
+                      </button>
+                  )}
+
+                  <button type="button" className="RtaskBtn" onClick={resetForm}>
+                    Reset task
+                  </button>
+                </div>
+
+              </form>
       </div>
   );
 }

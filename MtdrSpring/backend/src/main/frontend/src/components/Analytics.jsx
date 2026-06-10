285c5a4dce90e73032b7ecf9b07ea12f3c95ed32
Susu9878 <jalonzo9878@gmail.com>
2026-06-10 17:39:33 -0600

    graph color code fix

--- MtdrSpring/backend/src/main/frontend/src/components/Analytics.jsx
+++ MtdrSpring/backend/src/main/frontend/src/components/Analytics.jsx
@@ -215,7 +215,7 @@ function Analytics() {
       <div className="charts-section">
         <div className="charts-grid">
           <div className="sprintGrid">
-            <ChartContainer title="Daily Task Completion">
+            <ChartContainer title="Task Assignation">
               <ResponsiveContainer width="100%" height={350}>
                 <BarChart data={groupedFirst}>
                   <CartesianGrid strokeDasharray="3 3" />
@@ -227,6 +227,7 @@ function Analytics() {
                     <Bar
                       key={dev}
                       dataKey={dev}
+                      fill={`var(--bar-color-${index})`}
                       className={`bar-color-${index}`}
                     />
                   ))}

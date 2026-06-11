2f5e6955d69a0eb64adfe657818804fa2ff0cd48
Susu9878 <jalonzo9878@gmail.com>
2026-06-10 19:05:18 -0600

    users auth login implementation

---
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/AuthController.java
@@ -0,0 +1,46 @@
+package com.springboot.MyTodoList.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.MyTodoList.DTO.AuthResponseDTO;
+import com.springboot.MyTodoList.DTO.LoginRequestDTO;
+import com.springboot.MyTodoList.DTO.RegisterRequestDTO;
+import com.springboot.MyTodoList.service.AuthService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("todolist/api/auth")
+@CrossOrigin
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public AuthResponseDTO register(
+            @Valid @RequestBody RegisterRequestDTO request) {
+
+        String token = authService.register(request);
+
+        return new AuthResponseDTO(token, "User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public AuthResponseDTO login(
+            @Valid @RequestBody LoginRequestDTO request) {
+
+        String token = authService.login(request);
+
+        return new AuthResponseDTO(token, "Login successful");
+    }
+}
---
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/security/JwtUtil.java
@@ -0,0 +1,38 @@
+package com.springboot.MyTodoList.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+    private final String SECRET = "faL5Fp2UdDWgxOWxUWcDxmjxiXLBLJARj2o5nR8aCCG";
+
+    private final long EXPIRATION = 86400000; // 24h
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
